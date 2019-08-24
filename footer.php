8a903f4f6d0d7499c4c906b0165ad1f63b054413
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2019-08-24 03:11:41 -0300

    footer adicionado, design responsivo

---
+++ footer.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/f0f5a4c16a.js"></script>
+</head>
+<body>
+    
+    <footer class="bg-dark py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row">    
+                        <div class="accordion d-lg-flex w-100" id="accordion">
+                            <!-- ELEMENTO UM -->
+                            <div class="col-lg-4 col-md-12">
+                                <!-- ELEMENTO ANCORA -->
+                                <a href="#one" class="d-lg-none d-block h4 text-white nav-link p-0" data-toggle="collapse">
+                                    Links Utéis
+                                </a>
+                                <!-- TEXTO DISPLAY LG  -->
+                                <div class="text-white h4 d-none d-lg-block">
+                                    Links Utéis
+                                </div>
+                                <!-- CONTEUDO DA SECCAO -->
+                                <div class="collapse text-white d-lg-flex" data-parent="#accordion" id="one">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptatibus ipsam pariatur doloribus necessitatibus dolores nemo, aspernatur a accusantium perspiciatis ratione, magni sint. Voluptates eveniet quisquam vel. Officiis, temporibus quae.
+                                </div>
+                                <!-- FINAL DA DIV -->
+                            </div>
+                            <!-- ELEMENTO DOIS -->
+                            <div class="col-lg-4 col-md-12">
+                                <!-- ANCORA -->
+                                <a href="#two" class="d-lg-none d-block h4 text-white nav-link p-0" data-toggle="collapse">
+                                    Two
+                                </a>
+                                <!-- TEXTO DISPLAY LG -->
+                                <div class="text-white h4 d-none d-lg-block">
+                                    Two
+                                </div>
+                                <!-- CONTEUDO DA SECCAO -->
+                                <div class="collapse text-white d-lg-flex" data-parent="#accordion" id="two">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptatibus ipsam pariatur doloribus necessitatibus dolores nemo, aspernatur a accusantium perspiciatis ratione, magni sint. Voluptates eveniet quisquam vel. Officiis, temporibus quae.
+                                </div>
+                            </div>
+                            <!-- REDES SOCIAIS -->
+                            <div class="col-lg-4 col-md-12">
+                                <!-- ELEMENTO ANCORA  -->
+                                <a href="#three" class="d-lg-none d-block h4 text-white nav-link p-0" data-toggle="collapse">
+                                    Redes Sociais
+                                </a>
+                                <!-- TEXTO DISPLAY LG -->
+                                <div class="text-white h4 d-none d-lg-block">
+                                    Redes Sociais
+                                </div>
+                                <!-- CONTEUDO DA SECCAO -->
+                                <div class="collapse text-white d-lg-flex pt-2" data-parent="#accordion" id="three">
+                                    <ul class="list-unstyled d-flex w-100 justify-content-between" style="font-size: 24px;">
+                                        <li><a href="#" class="navlink py-2 px-4 border border-white"><i class="text-white fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#" class="navlink py-2 px-4 border border-white"><i class="text-white fab fa-twitter"></i></a></li>
+                                        <li><a href="#" class="navlink py-2 px-4 border border-white"><i class="text-white fab fa-instagram"></i></a></li>
+                                        <li><a href="#" class="navlink py-2 px-4 border border-white"><i class="text-white fab fa-youtube"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="container-fluid">
+        <div class="row bg-primary">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-center align-items-center">
+                    <span class="p-3 text-white">Pigtime &copy; Copyright 2019</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="js/footer.js"></script>
+</body>
+</html>
